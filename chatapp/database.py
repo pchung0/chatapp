592861ed6360c034062c69634fb8144697c9fbb8
Ping-Chun Chung <bingo61016@gmail.com>
2020-04-30 06:28:52 -0700

    add check to if username exist

--- chatapp/database.py
+++ chatapp/database.py
@@ -9,10 +9,15 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
     first_name = db.Column(db.String(), nullable=False)
     last_name = db.Column(db.String(), nullable=False)
-    username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('username is already taken')
 
     def __repr__(self):
         return f"User('{self.username}', '{self.first_name}', '{self.last_name}')"
