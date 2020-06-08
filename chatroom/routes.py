fb50494baace8ef551ab43e40d8c84c337771811
Ping-Chun Chung <bingo61016@gmail.com>
2020-06-07 19:32:00 -0700

    refactor

--- chatroom/routes.py
+++ chatroom/routes.py
@@ -5,84 +5,55 @@ from flask_socketio import join_room, leave_room, disconnect
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from chatroom import app, login_manager, db, socketio
-from chatroom.models import User, Room, Message
+from chatroom.models import User, Room
 from chatroom.socket import *
 
 
 @app.route('/')
 @login_required
 def home():
-    # rooms = [{'id':room.id, 'name':room.name} for room in current_user.rooms]
-    # return render_template('index2.html', rooms=rooms)
-    # return render_template('session.html')
     rooms = [{'id': room.id, 'name': room.name} for room in current_user.rooms]
     return render_template('room.html', current_room=None, rooms=rooms, messages=[])
 
 
-@app.route('/room/<path:new_room>', methods=['GET', 'POST'])
+@app.route('/room/<path:room_id>', methods=['GET', 'POST'])
 @login_required
-def enter_room(new_room):
+def enter_room(room_id):
     if 'room' in session:
         leave_room(session['room'], current_user.session_id, '/')
-    session['room'] = new_room
-    room = Room.query.filter_by(id=new_room).first()
+    session['room'] = room_id
+    room = Room.query.filter_by(id=room_id).first()
     if room and current_user in room.users:
-        join_room(new_room, current_user.session_id, '/')
+        join_room(room_id, current_user.session_id, '/')
         rooms = [{'id': room.id, 'name': room.name} for room in current_user.rooms]
-        # all_users = [{'id': id, 'username': username} for id, username in User.query.with_entities(User.id, User.username).all()]
-        room_users = {user.id for user in room.users}
-        users = []
-        for user in User.query.with_entities(User.id, User.username).all():
-            if user.id in room_users:
-                users.append({'id': user.id, 'username': user.username, 'is_room_member':True})
-            else:
-                users.append({'id': user.id, 'username': user.username, 'is_room_member':False})
-
-        # messages = [{'id': msg.id, 'message': msg.message, 'user_id': msg.user_id, 'datetime': msg.datetime} for msg in room.messages]
-        return render_template('room.html', users=users, current_room=room, rooms=rooms, messages=room.messages)
-        # return render_template('chatroom2.html', room_id=int(new_room), rooms=rooms, messages=messages)
+        return render_template('room.html', current_room=room)
     return redirect(url_for('home'))
 
 
-# @app.route('/create_room', methods=['POST'])
-# def create_room():
-#     if request.get_json():
-#         room_name = request.json['room_name']
-#         room = Room(name=room_name, owner_id=current_user.id)
-#         room.users.append(current_user)
-#         db.session.add(room)
-#         db.session.commit()
-
-#         join_room(room.id)
-#         socketio.emit('redirect room', room.id)
-#         socketio.send(f'{current_user.username} has entered the room.', room=room.id)
-#     return '1'
-
 @app.route('/room/<path:room_id>/messages', methods=['GET'])
 def room_message(room_id):
-    print(int(room_id))
-    print('************************')
     room = Room.query.filter_by(id=room_id).first()
     if room:
-        messages = [{'message':msg.message, 'user_id':msg.user_id, 'datetime':msg.datetime.strftime('%I:%M %p | %b %d')} for msg in room.messages]
+        messages = [{'message': msg.message, 'user_id': msg.user_id, 'datetime': msg.datetime.strftime(
+            '%I:%M %p | %b %d')} for msg in room.messages]
         return jsonify(messages)
     return '0'
+
 
 @app.route('/room_list', methods=['GET'])
 def room_list():
     rooms = [{'id': room.id, 'name': room.name} for room in current_user.rooms]
     return jsonify(rooms)
 
+
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.with_entities(User.id, User.username).all()
-    print('+++++++++++++++++++++++++')
-    print(users)
     return jsonify(users)
+
 
 @app.route('/room/<path:room_id>/not_members', methods=['GET'])
 def get_not_member_users(room_id):
-
     room = Room.query.filter_by(id=room_id).first()
     room_users = {user.id for user in room.users}
     users = []
@@ -91,6 +62,7 @@ def get_not_member_users(room_id):
             if user.id not in room_users:
                 users.append({'id': user.id, 'username': user.username})
         return jsonify(users)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
--- chatroom/static/script.js
+++ chatroom/static/script.js
@@ -1,7 +1,67 @@
 $(document).ready(function () {
-    console.log('{{ room_id }}')
     var socket = io.connect('http://' + document.domain + ':' + location.port);
-    var current_room_id = -1;
+    var current_room_id;
+    var current_room_name;
+    var current_user_id = $('#username').attr('data-user-id');
+
+    function init() {
+        render_room_list();
+        if (window.location.pathname.match(/^\/room\/[0-9]+$/g)) {
+            current_room_id = window.location.pathname.split("/").slice(-1)[0];
+            $('.invite-modal').removeClass('d-none');
+            render_messages();
+        }
+    }
+
+    $("#invite-input").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".dropdown-menu a").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    $('.dropdown-menu').find('a').click(function (e) {
+        e.preventDefault();
+        var param = $(this).html();
+        $('#invite-input').before('<span class="badge badge-secondary align-self-center mr-1">' + param + '</span>');
+        $('input').val('');
+        $('#invite-input').load("#invite-input");
+    });
+
+    $("#invite-input").keydown(function (e) {
+        if (e.which == 8 && $(this).val() == '')
+            $(this).prev().remove();
+    });
+
+    $('.modal').on('shown.bs.modal', function () {
+        $(this).find('input').focus();
+
+        if ($(this).attr('id') == 'invite-modal') {
+            $('#username-drop-down').empty();
+            get_not_in_room_users(current_room_id).then(users => {
+                console.log(users);
+                jQuery.each(users, function () {
+                    $('#username-drop-down').append('<a class="dropdown-item" href="#">' + this.username + '</a>')
+                });
+            });
+        }
+    })
+
+    $('div#room-list').on('click', 'a.room', function (e) {
+        e.preventDefault();
+        current_room_id = parseInt($(this).attr('href'));
+        current_room_name = $(this).attr('data-room-name');
+
+        $('#room-list a.active').addClass('list-group-item-light').removeClass('active');
+        $(this).addClass('active').removeClass('list-group-item-light');
+        $('div.chat-box').empty();
+
+        $('.current-room').text(current_room_name);
+        $('.invite-modal').removeClass('d-none');
+
+        render_messages();
+        history.pushState('data to be passed', '', '/room/' + current_room_id);
+    });
 
     socket.on('connect', function () {
         console.log('connected:')
@@ -12,26 +72,51 @@ $(document).ready(function () {
 
     var form = $('#message-form').on('submit', function (e) {
         e.preventDefault()
-        let user_input = $('#message-input-box').val()
-        socket.emit('send', {
-            user_name: '{{ current_user.username }}',
-            user_id: '{{ current_user.id }}',
-            room_id: '{{ room_id }}',
-            message: user_input
-        })
+        if (current_room_id) {
+            let user_input = $('#message-input-box').val()
+            if (user_input) {
+                socket.emit('send', {
+                    user_name: '{{ current_user.username }}',
+                    user_id: current_user_id,
+                    room_id: current_room_id,
+                    message: user_input
+                })
+            }
+        }
         $('#message-input-box').val('').focus()
-        $('div.chat-box').append(
-            `
-            <div class="media w-50 ml-auto mb-3">
-                <div class="media-body">
-                    <div class="bg-primary rounded py-2 px-3 mb-2">
-                        <p class="text-small mb-0 text-white">`+user_input+`</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                </div>
-            </div>
-            `)
     })
+
+    $("#invite").click(function () {
+        var users = [];
+        $(".input-holder span").each(function () { users.push($(this).text()) });
+        socket.emit('invite', {
+            room_id: current_room_id,
+            users: users
+        })
+        $('#invite-modal').modal('hide');
+    })
+
+    $('#create').click(function () {
+        let room_name = $('#create-room-input').val();
+        socket.emit('create', room_name);
+        $('#create-modal').modal('hide');
+        socket.on('redirect room', function (room_id) {
+            // $('div.message_holder').append('<div><b style="color: #000">' + msg + '</b></div>')
+            window.location.replace('http://' + document.domain + ':' + location.port + '/room/' + room_id);
+        })
+    })
+
+    $('#confirm-delete').on('show.bs.modal', function (e) {
+        $(this).find('.btn-ok').click(function () {
+            console.log(current_room_id);
+            socket.emit('delete', {
+                room_id: current_room_id,
+            })
+            $('#confirm-delete').modal('hide');
+            $('#room-list a.active').remove()
+        });
+
+    });
 
     $('#join').click(function () {
         let room = $('#room').val()
@@ -40,21 +125,9 @@ $(document).ready(function () {
         $('#room').val('')
     })
 
-    $('#create').click(function () {
-        let room = $('#room').val()
-        socket.emit('create', room)
-        // $('h2#roomname').text(room)
-        $('#room').val('')
-    })
-
     $("#leave").click(function () {
         socket.emit('leave')
         // $('h2#roomname').text('')
-    })
-
-    $(".room").click(function () {
-        current_room_id = $(this).text()
-        $('h2#roomname').text(current_room_id)
     })
 
     socket.on('room info', function (rooms) {
@@ -62,41 +135,130 @@ $(document).ready(function () {
         console.log(rooms)
     })
 
-    socket.on('message', function (data) {
+    socket.on('message', function (msg) {
         console.log("{{ url_for('home') }}" + '/room')
-        console.log('{{ room_id }}', data.room_id)
-        if ('{{ room_id }}' == data.room_id && '{{ current_user.id }}' != data.user_id) {
-            $('div.chat-box').append(
-            `
-            <div class="media w-50 mb-3"><img
-                    src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user"
-                    width="50" class="rounded-circle">
-                <div class="media-body ml-3">
-                    <div class="bg-light rounded py-2 px-3 mb-2">
-                        <p class="text-small mb-0 text-muted">`+data.message+`</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                </div>
-            </div>
-            `)
+        console.log(current_room_id, msg.room_id)
+        if (current_room_id == msg.room_id) {
+            if (current_user_id == msg.user_id)
+                append_receiver_chat_box(msg.message, msg.datetime);
+            else
+                append_sender_chat_box(msg.message, msg.datetime);
+            scroll_bottom('chat-box');
         }
     })
 
-    socket.on('my response', function (msg) {
-        if (typeof msg.user_name !== 'undefined') {
-            $('div.message_holder').append(
+    const get_room_list = async () => {
+        const response = await fetch('http://' + document.domain + ':' + location.port + '/room_list');
+        const room_list = await response.json();
+        return room_list;
+    }
+
+    const get_messages = async (room_id) => {
+        const response = await fetch('http://' + document.domain + ':' + location.port + '/room/' + room_id + '/messages');
+        const messages = await response.json();
+        return messages;
+    }
+
+    const get_users = async () => {
+        const response = await fetch('http://' + document.domain + ':' + location.port + '/users');
+        const users = await response.json();
+        return users;
+    }
+
+    const get_not_in_room_users = async (room_id) => {
+        const response = await fetch('http://' + document.domain + ':' + location.port + '/room/' + room_id + '/' + 'not_members');
+        const users = await response.json();
+        return users;
+    }
+
+    function render_messages() {
+        get_messages(current_room_id).then(messages => {
+            jQuery.each(messages, function () {
+                if (this.user_id == current_user_id)
+                    append_receiver_chat_box(this.message, this.datetime);
+                else
+                    append_sender_chat_box(this.message, this.datetime);
+            });
+        });
+    }
+
+    function render_room_list() {
+        get_room_list().then(rooms => {
+            jQuery.each(rooms, function () {
+                append_room_list(this.id, this.name, current_room_id == this.id);
+            });
+        });
+    }
+
+    function append_sender_chat_box(message, datetime) {
+        $('div.chat-box').append(
             `
             <div class="media w-50 mb-3"><img
                     src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user"
                     width="50" class="rounded-circle">
                 <div class="media-body ml-3">
                     <div class="bg-light rounded py-2 px-3 mb-2">
-                        <p class="text-small mb-0 text-muted">test</p>
+                        <p class="text-small mb-0 text-muted">` + message + `</p>
                     </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
+                    <p class="small text-muted">` + datetime + `</p>
                 </div>
             </div>
-            `)
+            `
+        );
+    }
+
+    function append_receiver_chat_box(message, datetime) {
+        $('div.chat-box').append(
+            `
+            <div class="media w-50 ml-auto mb-3">
+                <div class="media-body">
+                    <div class="bg-primary rounded py-2 px-3 mb-2">
+                        <p class="text-small mb-0 text-white">` + message + `</p>
+                    </div>
+                    <p class="small text-muted">` + datetime + `</p>
+                </div>
+            </div>
+            `
+        );
+    }
+
+    function append_room_list(id, name, activate) {
+        if (activate) {
+            $('div#room-list').append(
+                `
+                <a href="` + id + `" class="room active list-group-item list-group-item-action rounded-0">
+                    <div class="media">
+                        <div class="media-body ml-2">
+                            <div class="d-flex align-items-center justify-content-between mb-1">
+                                <h6 class="mb-0">` + name + `</h6>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                `
+            );
         }
-    })
+        else {
+            $('div#room-list').append(
+                `
+                <a href="` + id + `" class="room list-group-item list-group-item-action list-group-item-light rounded-0" data-room-id="` + id + `" data-room-name="` + name + `">
+                    <div class="media">
+                        <div class="media-body ml-2">
+                            <div class="d-flex align-items-center justify-content-between mb-1">
+                                <h6 class="mb-0">` + name + `</h6>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                `
+            );
+        }
+    }
+
+    function scroll_bottom(id) {
+        var chat_box = document.getElementById(id);
+        chat_box.scrollTop = chat_box.scrollHeight;
+    }
+
+    init();
 })
