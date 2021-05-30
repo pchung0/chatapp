570b13f7f1db865e256e6cf73bfca078e176a0b8
Ping-Chun Chung <bingo61016@gmail.com>
2021-05-29 18:28:11 -0700

    removed dead code and reorder functions

--- chatapp/static/js/script.js
+++ chatapp/static/js/script.js
@@ -6,6 +6,24 @@ $(document).ready(function () {
     var current_user_id = $('#username').attr('data-user-id');
     var current_username = $('#username').text();
 
+    socket.on('connect', function () {
+        socket.emit('my event', {
+            data: 'User Connected'
+        });
+    });
+
+    socket.on('message', function (msg) {
+        if (current_room_id == msg.room_id) {
+            if (current_username == msg.username)
+                append_receiver_chat_box(msg.message, msg.datetime);
+            else
+                append_sender_chat_box(msg.message, msg.datetime, msg.username);
+            scroll_bottom('chat-box');
+        } else {
+            $(`#room-list .room[data-room-id=${msg.room_id}] i`).removeClass('d-none');
+        }
+    });
+
     $("#invite-input").on("keyup", function (e) {
         var value = $(this).val().toLowerCase();
 
@@ -13,18 +31,18 @@ $(document).ready(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
 
-        $('.input-holder span').each(function(){
+        $('.input-holder span').each(function () {
             var username = $(this).text();
             $(`.dropdown-menu a:contains('${username}')`).toggle(false);
         });
     });
 
     $("#invite-input").on("keydown", function (e) {
-        if ($(this).val() == '' && e.which == 8 )
+        if ($(this).val() == '' && e.which == 8)
             $(this).prev().remove();
     });
 
-    $("#invite-input").focus(function(){
+    $("#invite-input").focus(function () {
         $(".dropdown").dropdown('show');
     });
 
@@ -69,12 +87,6 @@ $(document).ready(function () {
         history.pushState('data to be passed', '', `/rooms/${room_id}`);
     });
 
-    socket.on('connect', function () {
-        socket.emit('my event', {
-            data: 'User Connected'
-        });
-    });
-
     $('#message-form').on('submit', function (e) {
         e.preventDefault();
         if (current_room_id) {
@@ -96,16 +108,16 @@ $(document).ready(function () {
         var users = [];
         $(".input-holder span").each(function () { users.push($(this).text()); });
         $('#invite-modal').modal('hide');
-        invite_room(current_room_id, users).then(function(){
+        invite_room(current_room_id, users).then(function () {
             update_member_list(current_room_id);
         });
     });
 
-    $('#create-form').on('submit',function (e) {
+    $('#create-form').on('submit', function (e) {
         console.log('create');
         e.preventDefault();
         let room_name = $('#create-room-input').val();
-        create_room(room_name).then( room => {
+        create_room(room_name).then(room => {
             load_room_list(room.id)
             $('div.chat-box').empty();
             load_room(room.id);
@@ -136,23 +148,6 @@ $(document).ready(function () {
         socket.emit('leave');
     });
 
-    socket.on('room info', function (rooms) {
-        // $('div.message_holder').append('<div><b style="color: #000">' + msg + '</b></div>')
-        console.log(rooms);
-    });
-
-    socket.on('message', function (msg) {
-        if (current_room_id == msg.room_id) {
-            if (current_username == msg.username)
-                append_receiver_chat_box(msg.message, msg.datetime);
-            else
-                append_sender_chat_box(msg.message, msg.datetime, msg.username);
-            scroll_bottom('chat-box');
-        } else {
-            $(`#room-list .room[data-room-id=${msg.room_id}] i`).removeClass('d-none');
-        }
-    });
-
     function init() {
         let room_id = -1;
         if (window.location.pathname.match(/^\/rooms\/[0-9]+$/g)) {
@@ -162,7 +157,7 @@ $(document).ready(function () {
         load_room_list(room_id);
     }
 
-    function load_room(room_id){
+    function load_room(room_id) {
         $('.room-modal').removeClass('d-none');
         $(`#room-list .room[data-room-id= ${room_id}] i`).addClass('d-none'); // remove notification dot
 
@@ -170,7 +165,6 @@ $(document).ready(function () {
             current_room_id = room.id;
             current_room_name = room.name;
             current_room_owner = room.owner;
-
 
             room.users[0] = `${room.users[0]} (owner)`;
             $('#chatroom-title').attr('title', `Members:\n${room.users.join('\n')}`);
@@ -182,8 +176,8 @@ $(document).ready(function () {
         });
     }
 
-    function update_delete_leave_button(bool){
-        if (bool){
+    function update_delete_leave_button(bool) {
+        if (bool) {
             $('.delete-leave').text('Delete');
             $('i.fa-trash').removeClass('d-none');
             $('i.fa-sign-out').addClass('d-none');
@@ -194,12 +188,85 @@ $(document).ready(function () {
         }
     }
 
-    function update_member_list(room_id){
+    function update_member_list(room_id) {
         get_room_members(room_id).then(usernames => {
             usernames[0] = `${usernames[0]} (owner)`;
             $('#chatroom-title').attr('title', `Members:\n${room.users.join('\n')}`);
         });
+    }
 
+    function scroll_bottom(id) {
+        var chat_box = document.getElementById(id);
+        chat_box.scrollTop = chat_box.scrollHeight;
+    }
+
+    function render_messages(messages) {
+        jQuery.each(messages, function () {
+            if (this.username == current_username)
+                append_receiver_chat_box(this.message, this.datetime);
+            else
+                append_sender_chat_box(this.message, this.datetime, this.username);
+        });
+    }
+
+    function append_sender_chat_box(message, datetime, username) {
+        $('div.chat-box').append(
+            `
+            <div class="media w-50 mb-3"><img
+                    src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user"
+                    width="50" class="rounded-circle">
+                <div class="media-body ml-3">
+                    <div class="bg-light rounded py-2 px-3 mb-2">
+                        <p class="text-small font-weight-bold">${username}</p>
+                        <p class="text-small mb-0 text-muted">${message}</p>
+                    </div>
+                    <p class="small text-muted font-weight-light">${datetime}</p>
+                </div>
+            </div>
+            `
+        );
+    }
+
+    function append_receiver_chat_box(message, datetime) {
+        $('div.chat-box').append(
+            `
+            <div class="media w-50 ml-auto mb-3">
+                <div class="media-body">
+                    <div class="bg-primary rounded py-2 px-3 mb-2">
+                        <p class="text-small mb-0 text-white">${message}</p>
+                    </div>
+                    <p class="small text-muted">${datetime}</p>
+                </div>
+            </div>
+            `
+        );
+    }
+
+    function load_room_list(activate_room_id) {
+        $('div#room-list').empty();
+        get_room_list().then(rooms => {
+            jQuery.each(rooms, function () {
+                append_room_list(this.id, this.name, this.owner_id, activate_room_id == this.id);
+            });
+        });
+    }
+
+    function append_room_list(id, name, owner_id, activate) {
+        $('div#room-list').append(
+            `
+                <a href="${id}" class="room list-group-item list-group-item-action rounded-0 ${activate ? 'activate' : ''}"
+                data-room-id="${id}" data-room-name="${name}" data-room-owner-id="${owner_id}">
+                    <div class="media">
+                        <div class="media-body ml-2">
+                            <div class="d-flex align-items-center justify-content-between mb-1">
+                                <h6 class="mb-0">${name}</h6>
+                                <i class="fa fa-circle text-primary d-none" style="font-size:10px;"></i>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                `
+        );
     }
 
     const get_room_list = async () => {
@@ -245,108 +312,13 @@ $(document).ready(function () {
                 'Content-Type': 'application/json'
             }
         });
-        const myJson = await response.text();
     };
 
     const delete_room = async (room_id) => {
         const response = await fetch(`/api/rooms/${room_id}`, {
             method: 'DELETE'
         });
-        const myJson = await response.text();
     };
-
-    function render_messages(messages) {
-        jQuery.each(messages, function () {
-            if (this.username == current_username)
-                append_receiver_chat_box(this.message, this.datetime);
-            else
-                append_sender_chat_box(this.message, this.datetime, this.username);
-        });
-    }
-
-    function load_room_list(activate_room_id) {
-        $('div#room-list').empty();
-        get_room_list().then(rooms => {
-            jQuery.each(rooms, function () {
-                append_room_list(this.id, this.name, this.owner_id, activate_room_id == this.id);
-            });
-        });
-    }
-
-    function append_sender_chat_box(message, datetime, username) {
-        $('div.chat-box').append(
-            `
-            <div class="media w-50 mb-3"><img
-                    src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user"
-                    width="50" class="rounded-circle">
-                <div class="media-body ml-3">
-                    <div class="bg-light rounded py-2 px-3 mb-2">
-                        <p class="text-small font-weight-bold">${username}</p>
-                        <p class="text-small mb-0 text-muted">${message}</p>
-                    </div>
-                    <p class="small text-muted font-weight-light">${datetime}</p>
-                </div>
-            </div>
-            `
-        );
-    }
-
-    function append_receiver_chat_box(message, datetime) {
-        $('div.chat-box').append(
-            `
-            <div class="media w-50 ml-auto mb-3">
-                <div class="media-body">
-                    <div class="bg-primary rounded py-2 px-3 mb-2">
-                        <p class="text-small mb-0 text-white">${message}</p>
-                    </div>
-                    <p class="small text-muted">${datetime}</p>
-                </div>
-            </div>
-            `
-        );
-    }
-
-    function append_room_list(id, name, owner_id, activate) {
-        if (activate) {
-            $('div#room-list').append(
-                `
-                <a href="${id}" class="room active list-group-item list-group-item-action rounded-0"
-                data-room-id="${id}" data-room-name="${name}" data-room-owner-id="${owner_id}">
-                    <div class="media">
-                        <div class="media-body ml-2">
-                            <div class="d-flex align-items-center justify-content-between mb-1">
-                                <h6 class="mb-0">${name}</h6>
-                                <i class="fa fa-circle text-primary d-none" style="font-size:10px;"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                `
-            );
-        }
-        else {
-            $('div#room-list').append(
-                `
-                <a href="${id}" class="room list-group-item list-group-item-action list-group-item-light rounded-0"
-                    data-room-id="+ ${id}" data-room-name="${name}" data-room-owner-id="${owner_id}">
-                    <div class="media">
-                        <div class="media-body ml-2">
-                            <div class="d-flex align-items-center justify-content-between mb-1">
-                                <h6 class="mb-0">${name}</h6>
-                                <i class="fa fa-circle text-primary d-none" style="font-size:10px;"></i>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                `
-            );
-        }
-    }
-
-    function scroll_bottom(id) {
-        var chat_box = document.getElementById(id);
-        chat_box.scrollTop = chat_box.scrollHeight;
-    }
 
     init();
 });
